088c578319caf05c8f4672548a9a08d2ecb24662
LENOVO <cumody82@gmail.com>
2020-09-11 22:24:38 +0200

    RunBlocking

--- app/src/main/java/com/mahmoudshaaban/kotlincoroutines/MainActivity.kt
+++ app/src/main/java/com/mahmoudshaaban/kotlincoroutines/MainActivity.kt
@@ -16,29 +16,33 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        GlobalScope.launch(Dispatchers.IO) {
-            // Dispatchers.Unconfined is different: it executes coroutine immediately on the current thread
-            // and later resumes it in whatever thread called resume.
-            // It is usually a good fit for things like intercepting regular non-suspending API
-            // or invoking coroutine-related code from blocking world callbacks.
+        Log.d(TAG, "Before RunBlocking")
+        // RunBlocking is executed in main Thread
+        runBlocking {
+            Log.d(TAG, "Start RunBlocking")
 
-            Log.d(TAG, "current thread : ${Thread.currentThread().name}")
+            // we can put launch without GlobalScope b/c we are already in mainThread(RunBlocking)
+            launch(IO) {
 
-            val network = doNetWorkCall()
+                delay(5000L)
+                Log.d(TAG, "Finished IO Coroutine 1")
 
+            }
+            launch(IO) {
 
-            // Switching from a IO To Main
-            GlobalScope.launch(Dispatchers.Main) {
-                Log.d(TAG, "current thread : ${Thread.currentThread().name}")
-                text.text = network
+                delay(5000L)
+                Log.d(TAG, "Finished IO Coroutine 2")
+
             }
 
+
+            Log.d(TAG, "End RunBlocking")
+
         }
+        Log.d(TAG, "After RunBlocking")
+
+
     }
 
-    suspend fun doNetWorkCall(): String {
-        delay(3000L)
-        return "Network Call 1"
-    }
 
 }
